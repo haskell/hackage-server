fe51496d440f68db928c54e07c02deb2fc92f16d
Eelco Lempsink <eml@tupil.com>
2008-06-29 12:42:25 +0000

    Crude hacks to make it compile (with Cabal 1.4)

--- Hackage/Types.hs
+++ Hackage/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable, StandaloneDeriving #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Hackage.Types
@@ -15,7 +15,7 @@ module Hackage.Types where
 
 import Distribution.Package
          ( PackageIdentifier(..), Package(..), Dependency )
-import Distribution.PackageDescription (GenericPackageDescription)
+import Distribution.PackageDescription (GenericPackageDescription(..), parsePackageDescription, ParseResult(..))
 import Distribution.Text
          ( display )
 import Distribution.Simple.Utils (intercalate)
@@ -25,6 +25,14 @@ import System.FilePath ((</>), (<.>))
 
 type Username = String
 type Password = String
+
+instance Read GenericPackageDescription where
+    readsPrec _ s = case parsePackageDescription s of
+        ParseFailed e -> [(undefined, "")]
+        ParseOk _ x   -> [(x, "")]
+
+deriving instance Typeable GenericPackageDescription
+deriving instance Typeable PackageIdentifier
 
 -- | We re-use @GenericPackageDescription@ and use the @package-url@
 -- field to store the tarball URL.
