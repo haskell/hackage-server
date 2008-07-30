8127fed98f42fb4a427c2b1f510541214aed156d
Duncan Coutts <duncan@haskell.org>
2008-07-30 21:38:37 +0000

    Add rss feed generation, but not linked to a url yet

--- Distribution/Server/Pages/Recent.hs
+++ Distribution/Server/Pages/Recent.hs
@@ -2,7 +2,7 @@
 
 module Distribution.Server.Pages.Recent (
     recentPage,
---    recentFeed,
+    recentFeed,
   ) where
 
 import Distribution.Server.Types
@@ -11,39 +11,62 @@ import Distribution.Server.Pages.Template
          ( hackagePageWith )
 
 import Distribution.Package
-         ( PackageIdentifier )
+         ( PackageIdentifier, packageName, packageVersion )
+import Distribution.PackageDescription
+         ( GenericPackageDescription(packageDescription)
+         , PackageDescription(synopsis)  )
 import Distribution.Text
          ( display )
 
+import qualified Text.XHtml as XHtml
 import Text.XHtml
+         ( Html, URL, (<<), (!) )
+import qualified Text.RSS as RSS
+import Text.RSS
+         ( RSS(RSS) )
+import Network.URI
+         ( URI(..), URIAuth(..), uriToString )
+import Data.Time.Clock
+         ( UTCTime )
+import Data.Time.Clock.POSIX
+         ( utcTimeToPOSIXSeconds )
 import Data.Time.Format
          ( formatTime )
 import System.Locale
          ( defaultTimeLocale )
+import System.Time --sadly have to use the old-time lib as well as new :-(
+         ( CalendarTime, ClockTime(TOD), toUTCTime )
 
 -- | Takes a list of package info, in reverse order by timestamp.
 --
 recentPage :: [PkgInfo] -> Html
 recentPage pkgs =
   let log_rows = map makeRow (take 20 pkgs)
-      docBody = [h2 << "Recent additions",
-	  table ! [align "center"] << log_rows]
-      rss_link = thelink ! [rel "alternate",
-                            thetype "application/rss+xml",
-                            title "HackageDB RSS Feed",
-                            href rssFeedURL] << noHtml
+      docBody = [XHtml.h2 << "Recent additions",
+	  XHtml.table ! [XHtml.align "center"] << log_rows]
+      rss_link = XHtml.thelink ! [XHtml.rel "alternate",
+                                  XHtml.thetype "application/rss+xml",
+                                  XHtml.title "HackageDB RSS Feed",
+                                  XHtml.href rssFeedURL] << XHtml.noHtml
    in hackagePageWith [rss_link] "recent additions" docBody
 
 makeRow :: PkgInfo -> Html
 makeRow PkgInfo { pkgInfoId = pkgid
                 , pkgUploadTime = time, pkgUploadUser = user } =
-  tr <<
-    [td ! [align "right"] <<
-	    [toHtml (formatTime defaultTimeLocale "%c" time), nbsp, nbsp],
-     td ! [align "left"] << user,
-     td ! [align "left"] <<
-	    [nbsp, nbsp, anchor ! [href (packageURL pkgid)] << display pkgid]]
-  where nbsp = primHtmlChar "nbsp"
+  XHtml.tr <<
+    [XHtml.td ! [XHtml.align "right"] <<
+	    [XHtml.toHtml (showTime time), nbsp, nbsp],
+     XHtml.td ! [XHtml.align "left"] << user,
+     XHtml.td ! [XHtml.align "left"] <<
+	    [nbsp, nbsp, XHtml.anchor !
+                           [XHtml.href (packageURL pkgid)] << display pkgid]]
+  where nbsp = XHtml.primHtmlChar "nbsp"
+
+convertTime :: UTCTime -> CalendarTime
+convertTime utc = toUTCTime (TOD (floor (utcTimeToPOSIXSeconds utc)) 0)
+
+showTime :: UTCTime -> String
+showTime = formatTime defaultTimeLocale "%c"
 
 -- | URL describing a package.
 packageURL :: PackageIdentifier -> URL
@@ -51,3 +74,49 @@ packageURL pkgid = "/packages/" ++ display pkgid
 
 rssFeedURL :: URL
 rssFeedURL = "/packages/recent.rss"
+
+recentAdditionsURL :: URL
+recentAdditionsURL = "/packages/recent.html"
+
+recentFeed :: UTCTime -> [PkgInfo] -> RSS.RSS
+recentFeed now pkgs = RSS.RSS
+  "Recent additions"
+  (hackageURI recentAdditionsURL)
+  desc
+  (channel now)
+  (map releaseItem pkgs)
+  where
+    desc = "The 20 most recent additions to HackageDB, the Haskell package database."
+
+hackageURI :: String -> URI
+hackageURI path =
+	URI "http:" (Just (URIAuth "" "hackage.haskell.org" "")) path "" ""
+
+channel :: UTCTime -> [RSS.ChannelElem]
+channel now =
+  [ RSS.Language "en"
+  , RSS.ManagingEditor email
+  , RSS.WebMaster email
+  , RSS.ChannelPubDate now'
+  , RSS.LastBuildDate	now'
+  , RSS.Generator "rss-feed"
+  ]
+  where
+    email = "Ross Paterson <ross@soi.city.ac.uk>"
+    now'  = convertTime now
+
+releaseItem :: PkgInfo -> [RSS.ItemElem]
+releaseItem PkgInfo { pkgInfoId = pkgId, pkgDesc = pkg
+                    , pkgUploadTime = time, pkgUploadUser = user } =
+  [ RSS.Title title
+  , RSS.Link uri
+  , RSS.Guid True (uriToString id uri "")
+  , RSS.PubDate (convertTime time)
+  , RSS.Description desc
+  ]
+  where
+    uri   = hackageURI (packageURL pkgId)
+    title = packageName pkgId ++ " " ++ display (packageVersion pkgId)
+    body  = synopsis (packageDescription pkg)
+    desc  = "<i>Added by " ++ user ++ ", " ++ showTime time ++ ".</i>"
+	 ++ if null body then "" else "<p>" ++ body
