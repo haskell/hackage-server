71492179fd7100113bac2788cf7a41b86fe593df
Duncan Coutts <duncan@well-typed.com>
2011-07-23 21:18:04 +0000

    Allow listing specific packages to mirror on the command line

--- MirrorClient.hs
+++ MirrorClient.hs
@@ -10,6 +10,7 @@ import Distribution.Server.Users.Types (UserId(..), UserName(UserName))
 import Distribution.Server.Util.Index as PackageIndex (read)
 import Distribution.Server.Util.Merge
 import Distribution.Package
+import Distribution.Version
 import Distribution.Text
 import Distribution.Verbosity
 import Distribution.Simple.Utils
@@ -37,9 +38,10 @@ import Debug.Trace
 
 
 data MirrorOpts = MirrorOpts {
-                    srcURI   :: URI,
-                    dstURI   :: URI,
-                    stateDir :: FilePath
+                    srcURI       :: URI,
+                    dstURI       :: URI,
+                    stateDir     :: FilePath,
+                    selectedPkgs :: [PackageId]
                   }
 
 data PkgMirrorInfo = PkgMirrorInfo
@@ -72,7 +74,12 @@ mirrorOnce verbosity opts = do
       srcIndex <- downloadIndex (srcURI opts) srcCacheFile
       dstIndex <- downloadIndex (dstURI opts) dstCacheFile
 
-      let pkgsToMirror = diffIndex srcIndex dstIndex
+      let pkgsMissingFromDest = diffIndex srcIndex dstIndex
+          pkgsToMirror
+            | null (selectedPkgs opts) = pkgsMissingFromDest
+            | otherwise                = subsetIndex (selectedPkgs opts)
+                                                     pkgsMissingFromDest
+
       ioAction $ notice verbosity $ show (length pkgsToMirror) ++ " packages to mirror."
 
       mirrorPackages verbosity opts pkgsToMirror
@@ -91,6 +98,20 @@ diffIndex as bs =
                                        (sortBy (comparing mirrorPkgId) bs) ]
   where
     mirrorPkgId (PkgMirrorInfo pkgid _ _ _) = pkgid
+
+subsetIndex :: [PackageId] -> [PkgMirrorInfo] -> [PkgMirrorInfo]
+subsetIndex pkgids =
+    filter (\(PkgMirrorInfo pkgid' _ _ _) -> anyMatchPackage pkgids pkgid')
+  where
+    anyMatchPackage :: [PackageId] -> PackageId -> Bool
+    anyMatchPackage pkgids pkgid' =
+      any (\pkgid -> matchPackage pkgid pkgid') pkgids
+
+    matchPackage :: PackageId -> PackageId -> Bool
+    matchPackage (PackageIdentifier name  (Version [] _))
+                 (PackageIdentifier name' _             ) = name == name'
+    matchPackage pkgid pkgid' = pkgid == pkgid'
+
 
 mirrorPackages :: Verbosity -> MirrorOpts -> [PkgMirrorInfo] -> HttpSession ()
 mirrorPackages verbosity opts pkgsToMirror = do
@@ -343,7 +364,7 @@ mirrorFlagDescrs =
 
   , Option ['v'] []
       (NoArg (\opts -> opts { flagVerbosity = moreVerbose (flagVerbosity opts) }))
-      "Verbose mode"
+      "Verbose mode (can be listed multiple times e.g. -vv)"
 
   , Option [] ["cache-dir"]
       (ReqArg (\dir opts -> opts { flagCacheDir = Just dir }) "DIR")
@@ -367,14 +388,22 @@ validateOpts args = do
     when (not (null errs)) (printErrors errs)
 
     case args' of
-      [from, to] -> case (validateURI from, validateURI to) of
+      (from:to:pkgstrs) -> case (validateURI from, validateURI to) of
         (Left err, _) -> die err
         (_, Left err) -> die err
-        (Right fromURI, Right toURI) ->
+        (Right fromURI, Right toURI) -> do
+          let pkgstrs' = [ (pkgstr, simpleParse pkgstr) | pkgstr <- pkgstrs ]
+              pkgs     = [ pkgid  | (_, Just pkgid)   <- pkgstrs' ]
+              errs     = [ pkgstr | (pkgstr, Nothing) <- pkgstrs' ]
+          case errs of
+            (err:_) -> die $ "'" ++ err ++ "' is not a valid package name or id"
+            _       -> return ()
+
           return $ (,) (flagVerbosity flags) MirrorOpts {
-            srcURI   = fromURI,
-            dstURI   = toURI,
-            stateDir = fromMaybe "mirror-cache" (flagCacheDir flags)
+            srcURI       = fromURI,
+            dstURI       = toURI,
+            stateDir     = fromMaybe "mirror-cache" (flagCacheDir flags),
+            selectedPkgs = pkgs
           }
 
       _ -> do putStrLn "expected two URLs, a source and destination"
@@ -384,7 +413,7 @@ validateOpts args = do
     printUsage  = do
       putStrLn $ usageInfo usageHeader mirrorFlagDescrs
       exitSuccess
-    usageHeader = "Usage: hackage-mirror fromURL toURL [options]\nOptions:"
+    usageHeader = "Usage: hackage-mirror fromURL toURL [packages] [options]\nOptions:"
     printErrors errs = do
       putStrLn $ concat errs ++ "Try --help."
       exitFailure
