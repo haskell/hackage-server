2dba19732fb21bb3ff3d8b0c4dcc7e273be418c6
Duncan Coutts <duncan@community.haskell.org>
2011-08-06 11:12:31 +0000

    Add the missing Framework module

---
+++ Distribution/Server/Framework.hs
@@ -0,0 +1,23 @@
+-- | Re-export the common parts of the server framework.
+--
+module Distribution.Server.Framework (
+
+    module Happstack.Server,
+    module Distribution.Server.Framework.Feature,
+    module Distribution.Server.Framework.Types,
+    module Distribution.Server.Framework.ResourceTypes,
+    module Distribution.Server.Framework.Resource,
+    module Distribution.Server.Framework.Hook,
+    module Distribution.Server.Framework.Error,
+    module Distribution.Server.Util.Happstack
+
+  ) where
+
+import Happstack.Server
+import Distribution.Server.Framework.Feature
+import Distribution.Server.Framework.Types
+import Distribution.Server.Framework.ResourceTypes
+import Distribution.Server.Framework.Resource
+import Distribution.Server.Framework.Hook
+import Distribution.Server.Framework.Error
+import Distribution.Server.Util.Happstack
