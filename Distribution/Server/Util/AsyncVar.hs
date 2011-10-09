1dcb855ca962ff89b550153f940c66a88e228509
Duncan Coutts <duncan@community.haskell.org>
2011-10-09 16:29:55 +0000

    Rework the async var to batch updates and amortise the work Previously, every single write to the var was laboriously evaluated and since jobs keep comming in faster than they can be evaluated, then the input queue just grows without bound. But if we're just writing then if we have several new inputs in the queue then we can just skip to the final update and evaluate that one. That's now what we do, with a slight complication to do with incremental updates.

--- Distribution/Server/Util/AsyncVar.hs
+++ Distribution/Server/Util/AsyncVar.hs
@@ -7,36 +7,54 @@ module Distribution.Server.Util.AsyncVar (
   ) where
 
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Exception
 
-import Prelude hiding (read)
+import Prelude hiding (read, catch)
 
-data AsyncVar state = AsyncVar !(Chan (Either (state -> state) state)) !(MVar state)
+data AsyncVar state = AsyncVar !(TChan (Either (state -> state) state))
+                               !(MVar state)
 
 new :: (state -> ()) -> state -> IO (AsyncVar state)
 new force initial = do
-    evaluate (force initial)
-    inChan  <- newChan
+    () <- evaluate (force initial)
+    parent <- myThreadId
+    inChan <- atomically $ newTChan
     outVar <- newMVar initial
     let loop = do
-        -- TODO: catch exceptions.. perhaps..
-        next <- readChan inChan
-        current <- takeMVar outVar
-        let value = case next of
-                Left func -> func current
-                Right val -> val
-        evaluate (force value)
-        putMVar outVar value
-        loop
+        avail   <- readAllAvailable inChan
+        current <- readMVar outVar
+        -- we have a series of updates, either incremental or replacement
+        let value = foldl accum current avail -- note: not foldl' on purpose!
+            accum x (Left  f)  = f x
+            accum _ (Right x') = x'
+        res <- try $ evaluate (force value)
+        case res of
+          Left  e -> do throwTo parent (e :: SomeException)
+                        loop
+          Right _ -> do modifyMVar_ outVar (\_ -> return value)
+                        loop
     forkIO loop
     return (AsyncVar inChan outVar)
+  where
+    -- get a list of all the input states currently queued
+    readAllAvailable chan =
+      atomically $ do
+        x <- readTChan chan -- will block if queue is empty
+        readAll [x]         -- will never block, just gets what's available
+      where
+        readAll xs = do
+          empty <- isEmptyTChan chan
+          if empty
+            then return (reverse xs)
+            else do x <- readTChan chan
+                    readAll (x:xs)
 
 read :: AsyncVar state -> IO state
 read (AsyncVar _ outVar) = readMVar outVar
 
 write :: AsyncVar state -> state -> IO ()
-write (AsyncVar inChan _) value = writeChan inChan (Right value)
+write (AsyncVar inChan _) value = atomically $ writeTChan inChan (Right value)
 
 modify :: AsyncVar state -> (state -> state) -> IO ()
-modify (AsyncVar inChan _) func = writeChan inChan (Left func)
-
+modify (AsyncVar inChan _) func = atomically $ writeTChan inChan (Left func)
