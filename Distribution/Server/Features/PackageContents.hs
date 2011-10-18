e8825bafe1552efe36dc031d8e867a4fffe2f919
Max Bolingbroke <batterseapower@hotmail.com>
2011-10-18 09:44:52 +0000

    Serve candidate package contents as well

--- Distribution/Server/Features/PackageContents.hs
+++ Distribution/Server/Features/PackageContents.hs
@@ -29,6 +29,7 @@ data PackageContentsFeature = PackageContentsFeature {
 
 data PackageContentsResource = PackageContentsResource {
     packageContents                   :: Resource,
+    packageContentsCandidate          :: Resource,
     packageContentsChangeLog          :: Resource,
     packageContentsCandidateChangeLog :: Resource,
 
@@ -44,7 +45,8 @@ initPackageContentsFeature env _ _ = do
     let store = serverBlobStore env
         resources = PackageContentsResource {
               packageContents                   = (resourceAt "/package/:package/src/..") { resourceGet = [("", serveContents store)] }
-            , packageContentsChangeLog          = (resourceAt "/package/:package/changelog") { resourceGet = [("changelog", runServerPartE . serveChangeLog store)] }
+            , packageContentsCandidate          = (resourceAt "/package/:package/candidate/src/..") { resourceGet = [("", serveCandidateContents store)] }
+            , packageContentsChangeLog          = (resourceAt "/package/:package/changelog") { resourceGet = [("changelog", serveChangeLog store)] }
             , packageContentsCandidateChangeLog = (resourceAt "/package/:package/candidate/changelog") { resourceGet = [("changelog", serveCandidateChangeLog store)] }
 
             , packageContentsChangeLogUri          = \pkgid -> renderResource (packageContentsChangeLog          resources) [display pkgid, display (packageName pkgid)]
@@ -52,40 +54,45 @@ initPackageContentsFeature env _ _ = do
           }
     return PackageContentsFeature {
         featureInterface = (emptyHackageFeature "package-contents") {
-          featureResources = map ($ resources) [packageContents, packageContentsChangeLog, packageContentsCandidateChangeLog]
+          featureResources = map ($ resources) [packageContents, packageContentsCandidate, packageContentsChangeLog, packageContentsCandidateChangeLog]
         }
       , packageContentsResource = resources
       }
 
 
+--TODO: use something other than runServerPartE for nice html error pages
+
+withPackagePath', withCandidatePath' :: DynamicPath -> (PkgInfo -> ServerPartE a) -> ServerPartE a
+withPackagePath'   dpath k = withPackagePath   dpath $ \pkg _ -> k pkg
+withCandidatePath' dpath k = withCandidatePath dpath $ \_ pkg -> k (candPkgInfo pkg)
+
 -- result: changelog or not-found error
-serveChangeLog :: BlobStorage -> DynamicPath -> ServerPartE Response
-serveChangeLog store dpath = withPackagePath dpath $ \pkg _ -> do
+serveChangeLog, serveCandidateChangeLog :: BlobStorage -> DynamicPath -> ServerPart Response
+serveChangeLog = serveChangeLog' withPackagePath'
+serveCandidateChangeLog = serveChangeLog' withCandidatePath'
+
+serveChangeLog' :: (DynamicPath -> ((PkgInfo -> ServerPartE Response) -> ServerPartE Response))
+                -> BlobStorage -> DynamicPath -> ServerPart Response
+serveChangeLog' with_pkg_path store dpath = runServerPartE $ with_pkg_path dpath $ \pkg -> do
     res <- liftIO $ lookupChangeLog store pkg
     case res of
       Left err -> errNotFound "Changelog not found" [MText err]
       Right (fp, offset, name) -> liftIO $ TarIndex.serveTarEntry fp offset name
 
-serveCandidateChangeLog :: BlobStorage -> DynamicPath -> ServerPart Response
-serveCandidateChangeLog store dpath =
-    runServerPartE $ --TODO: use something else for nice html error pages
-    withCandidatePath dpath $ \_ pkg -> do
-          res <- liftIO $ lookupChangeLog store (candPkgInfo pkg)
-          case res of
-            Left err -> errNotFound "Changelog not found" [MText err]
-            Right (fp, offset, name) -> liftIO $ TarIndex.serveTarEntry fp offset name
-
 -- return: not-found error or tarball
-serveContents :: BlobStorage -> DynamicPath -> ServerPart Response
-serveContents store dpath = runServerPartE $ withContents store dpath $ \pkgid tarball index -> do
+serveContents, serveCandidateContents :: BlobStorage -> DynamicPath -> ServerPart Response
+serveContents = serveContents' withPackagePath'
+serveCandidateContents = serveContents' withCandidatePath'
+
+serveContents' :: (DynamicPath -> ((PkgInfo -> ServerPartE Response) -> ServerPartE Response))
+               -> BlobStorage -> DynamicPath -> ServerPart Response
+serveContents' with_pkg_path store dpath = runServerPartE $ withContents $ \pkgid tarball index -> do
     -- if given a directory, the default page is index.html
     -- the default directory prefix is the package name itself (including the version)
     TarIndex.serveTarball ["index.html"] (display pkgid) tarball index
-
-withContents :: BlobStorage -> DynamicPath -> (PackageId -> FilePath -> TarIndex -> ServerPartE a) -> ServerPartE a
-withContents store dpath func =
-    withPackagePath dpath $ \pkg _ -> do
-    let pkgid = packageId pkg
-    case lookupTarball store pkg of
-        Nothing -> fail "Could not serve package contents: no tarball exists."
-        Just io -> liftIO io >>= \(fp, index) -> func pkgid fp index
+  where
+    withContents :: (PackageId -> FilePath -> TarIndex -> ServerPartE Response) -> ServerPartE Response
+    withContents func = with_pkg_path dpath $ \pkg ->
+        case lookupTarball store pkg of
+            Nothing -> fail "Could not serve package contents: no tarball exists."
+            Just io -> liftIO io >>= \(fp, index) -> func (packageId pkg) fp index
