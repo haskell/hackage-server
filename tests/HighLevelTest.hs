27d1c56edb5f79fbe7c2fcf92428af818e755bc7
Ian Lynagh <igloo@earth.li>
2012-07-17 14:53:19 +0000

    Expand the HighLevelTest

--- tests/HighLevelTest.hs
+++ tests/HighLevelTest.hs
@@ -12,7 +12,9 @@ import Control.Exception
 import Control.Monad
 import Data.Char
 import Data.List
+import Data.Maybe
 import Network.HTTP
+import Network.HTTP.Auth
 import System.Directory
 import System.Exit
 import System.FilePath
@@ -59,40 +61,113 @@ doit root
                            info "Finished")
 
 runTests :: IO ()
-runTests = do do info "Getting user list"
-                 xs <- getUrlStrings "/users/"
-                 unless (xs == ["Hackage users","admin"]) $
-                     error ("Bad user list: " ++ show xs)
-              do info "Getting admin user list"
-                 xs <- getUrlStrings "/users/admins/"
-                 unless (xs == ["Hackage admins","[","edit","]","admin"]) $
-                     error ("Bad admin user list: " ++ show xs)
-              do info "Creating user"
-                 postToUrl "/users/" [("username", "testuser"),
-                                      ("password", "testpass"),
-                                      ("repeat-password", "testpass")]
-              do info "Getting user list"
-                 xs <- getUrlStrings "/users/"
-                 unless (xs == ["Hackage users","admin","testuser"]) $
-                     error ("Bad user list: " ++ show xs)
+runTests = do
+    do info "Getting user list"
+       xs <- getUrlStrings "/users/"
+       unless (xs == ["Hackage users","admin"]) $
+           die ("Bad user list: " ++ show xs)
+    do info "Getting admin user list"
+       xs <- getUrlStrings "/users/admins/"
+       unless (xs == ["Hackage admins","[","edit","]","admin"]) $
+           die ("Bad admin user list: " ++ show xs)
+    do info "Creating user"
+       postToUrl "/users/" [("username", "testuser"),
+                            ("password", "testpass"),
+                            ("repeat-password", "testpass")]
+    do info "Creating user2"
+       postToUrl "/users/" [("username", "testuser2"),
+                            ("password", "testpass2"),
+                            ("repeat-password", "testpass2")]
+    do info "Checking new users are now in user list"
+       xs <- getUrlStrings "/users/"
+       unless (xs == ["Hackage users","admin","testuser", "testuser2"]) $
+           die ("Bad user list: " ++ show xs)
+    do info "Getting password change page for testuser"
+       void $ getAuthUrlStrings "testuser" "testpass" "/user/testuser/password"
+    do info "Getting password change page for testuser as an admin"
+       void $ getAuthUrlStrings "admin" "admin" "/user/testuser/password"
+    do info "Getting password change page for testuser as another user"
+       checkUnauthedUrl "testuser2" "testpass2" "/user/testuser/password"
+
+getUrlStrings :: String -> IO [String]
+getUrlStrings url = getUrlStrings' (getRequest (mkUrl url))
+
+getAuthUrlStrings :: String -> String -> String -> IO [String]
+getAuthUrlStrings u p url = withAuth u p url getUrlStrings'
+
+checkUnauthedUrl :: String -> String -> String -> IO ()
+checkUnauthedUrl u p url = withAuth u p url $ \req -> do
+    res <- simpleHTTP req
+    case res of
+        Left e ->
+            die ("Request failed: " ++ show e)
+        Right rsp
+         | rspCode rsp == (4, 0, 3) ->
+            return ()
+         | otherwise ->
+            die ("Bad response code: " ++ show (rspCode rsp))
+
+withAuth :: String -> String -> String -> (Request_String -> IO a) -> IO a
+withAuth u p url f = do
+    let req = getRequest (mkUrl url)
+    res <- simpleHTTP req
+    case res of
+        Left e ->
+            die ("Request failed: " ++ show e)
+        Right rsp
+         | rspCode rsp == (4, 0, 1) ->
+            do let uri = rqURI req
+                   hdrs = retrieveHeaders HdrWWWAuthenticate rsp
+                   challenges = catMaybes $ map (headerToChallenge uri) hdrs
+               auth <- case challenges of
+                       [] -> die "No challenges"
+                       c : _ ->
+                           case c of
+                           ChalBasic r ->
+                               return $ AuthBasic {
+                                            auSite = uri,
+                                            auRealm = r,
+                                            auUsername = u,
+                                            auPassword = p
+                                        }
+                           ChalDigest r d n o _stale a q ->
+                               return $ AuthDigest {
+                                            auRealm = r,
+                                            auUsername = u,
+                                            auPassword = p,
+                                            auDomain = d,
+                                            auNonce = n,
+                                            auOpaque = o,
+                                            auAlgorithm = a,
+                                            auQop = q
+                                        }
+               let req' = insertHeader HdrAuthorization
+                                       (withAuthority auth req)
+                                       req
+               f req'
+         | otherwise ->
+            die ("Bad response code: " ++ show (rspCode rsp))
 
 -- This is a simple HTML screen-scraping function. It skips down to
 -- a div with class "content", and then returns the list of strings
 -- that are in the following HTML.
-getUrlStrings :: String -> IO [String]
-getUrlStrings url
-    = do res <- simpleHTTP (getRequest (mkUrl url))
+getUrlStrings' :: Request_String -> IO [String]
+getUrlStrings' req
+    = do res <- simpleHTTP req
          case res of
              Left e ->
-                 error ("Request failed: " ++ show e)
+                 die ("Request failed: " ++ show e)
              Right rsp
               | rspCode rsp == (2, 0, 0) ->
                  do let xs0 = lines $ rspBody rsp
-                        xs1 = dropWhile (not . ("<div class=\"content\"" `isSuffixOf`)) xs0
-                        trim = dropWhile isSpace . reverse . dropWhile isSpace . reverse
-                    return $ filter (not . null) $ map trim $ getStrings 1 $ unlines xs1
+                        contentStart = ("<div class=\"content\"" `isSuffixOf`)
+                        xs1 = dropWhile (not . contentStart) xs0
+                        trim = dropWhile isSpace . reverse
+                             . dropWhile isSpace . reverse
+                        tidy = filter (not . null) . map trim
+                    return $ tidy $ getStrings 1 $ unlines xs1
               | otherwise ->
-                 error ("Bad response code: " ++ show (rspCode rsp))
+                 die ("Bad response code: " ++ show (rspCode rsp))
     where isAngleBracket '<' = True
           isAngleBracket '>' = True
           isAngleBracket _   = False
@@ -114,12 +189,12 @@ postToUrl url vals
          res <- simpleHTTP req
          case res of
              Left e ->
-                 error ("Request failed: " ++ show e)
+                 die ("Request failed: " ++ show e)
              Right rsp
               | rspCode rsp == (3, 0, 3) ->
                  return ()
               | otherwise ->
-                 error ("Bad response code: " ++ show (rspCode rsp))
+                 die ("Bad response code: " ++ show (rspCode rsp))
 
 mkUrl :: String -> String
 mkUrl path = "http://127.0.0.1:" ++ show testPort ++ path
@@ -134,12 +209,11 @@ waitForServer = f 10
                         | rspCode rsp == (2, 0, 0) ->
                            info "Server is up"
                        _ ->
-                           if n == 0
-                           then error "Server didn't come up"
-                           else do info "Server not up yet; will try again shortly"
-                                   info ("(result was " ++ show res ++ ")")
-                                   threadDelay 100000
-                                   f (n - 1)
+                           do when (n == 0) $ die "Server didn't come up"
+                              info "Server not up yet; will try again shortly"
+                              info ("(result was " ++ show res ++ ")")
+                              threadDelay 100000
+                              f (n - 1)
 
 runServer :: FilePath -> [String] -> IO (Maybe ExitCode)
 runServer root args = run server args'
@@ -149,4 +223,8 @@ runServer root args = run server args'
 
 info :: String -> IO ()
 info str = putStrLn ("= " ++ str)
+
+die :: String -> IO a
+die err = do hPutStrLn stderr err
+             exitFailure
 
