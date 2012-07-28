df7d4ad32801516c54833265392987b1c010b785
Ian Lynagh <ian@well-typed.com>
2012-07-28 21:34:19 +0000

    Refactor the doc builder

--- BuildClient.hs
+++ BuildClient.hs
@@ -30,47 +30,108 @@ import System.Process
 import System.IO.Error
 
 
--- FIXMEs:
---  * Sandbox user-supplied code (i.e. the whole 'cabal install' invocation)
-
+data Mode = Help [String]
+          | Init String
+          | Build [PackageId]
 
 data BuildOpts = BuildOpts {
-                    srcURI       :: URI,
-                    username     :: String,
-                    password     :: String,
-                    stateDir     :: FilePath,
-                    selectedPkgs :: [PackageId]
-                  }
+                     bo_verbosity :: Verbosity,
+                     bo_stateDir  :: FilePath
+                 }
 
-srcName :: BuildOpts -> String
-srcName opts = fromMaybe (show (srcURI opts)) (uriHostName (srcURI opts))
+data BuildConfig = BuildConfig {
+                       bc_srcURI   :: URI,
+                       bc_username :: String,
+                       bc_password :: String
+                   }
+
+srcName :: BuildConfig -> String
+srcName config = fromMaybe (show (bc_srcURI config))
+                           (uriHostName (bc_srcURI config))
 
 
 main :: IO ()
 main = topHandler $ do
     args <- getArgs
-    (verbosity, opts) <- validateOpts args
+    (mode, opts) <- validateOpts args
 
-    buildOnce verbosity opts
+    case mode of
+        Help strs ->
+            do let usageHeader = intercalate "\n" [
+                       "Usage: hackage-build init URL [options]",
+                       "Usage: hackage-build build [packages] [options]",
+                       "Options:"]
+               mapM_ putStrLn $ strs
+               putStrLn $ usageInfo usageHeader buildFlagDescrs
+               unless (null strs) exitFailure
+        Init uri -> initialise opts uri
+        Build pkgs ->
+            do stateDir <- canonicalizePath $ bo_stateDir opts
+               let opts' = opts {
+                               bo_stateDir = stateDir
+                           }
+               buildOnce opts' pkgs
 
 
-buildOnce :: Verbosity -> BuildOpts -> IO ()
-buildOnce verbosity opts = do
-    (does_not_have_docs, mark_as_having_docs, persist) <- mkPackageHasDocs opts
+initialise :: BuildOpts -> String -> IO ()
+initialise opts uriStr
+    = do putStrLn "Enter hackage username"
+         username <- getLine
+         putStrLn "Enter hackage password"
+         password <- getLine
+         createDirectoryIfMissing False $ bo_stateDir opts
+         -- [Note: Show/Read URI]
+         -- Ideally we'd just be showing a BuildConfig, but URI doesn't
+         -- have Show/Read, so that doesn't work. So instead, we write
+         -- out a tuple containing the uri as a string, and parse it
+         -- each time we read it.
+         writeFile (configFile opts) (show (uriStr, username, password))
+
+readConfig :: BuildOpts -> IO BuildConfig
+readConfig opts = do xs <- readFile $ configFile opts
+                     case reads xs of
+                         [((uriStr, username, password), "")] ->
+                             case validateHackageURI uriStr of
+                             -- Shouldn't happen: We check that this
+                             -- returns Right when we create the
+                             -- config file. See [Note: Show/Read URI].
+                             Left err -> die err
+                             Right uri ->
+                                 return $ BuildConfig {
+                                              bc_srcURI   = uri,
+                                              bc_username = username,
+                                              bc_password = password
+                                          }
+                         _ ->
+                             die "Can't parse config file"
+
+configFile :: BuildOpts -> FilePath
+configFile opts = bo_stateDir opts </> "hd-config"
+
+buildOnce :: BuildOpts -> [PackageId] -> IO ()
+buildOnce opts pkgs = do
+    config <- readConfig opts
+    let verbosity = bo_verbosity opts
+        cacheDir = bo_stateDir opts </> cacheDirName
+        cacheDirName | URI { uriAuthority = Just auth } <- bc_srcURI config
+                     = makeValid (uriRegName auth ++ uriPort auth)
+                     | otherwise
+                     = error $ "unexpected URI " ++ show (bc_srcURI config)
+
+    (does_not_have_docs, mark_as_having_docs, persist) <- mkPackageHasDocs opts config
 
     createDirectoryIfMissing False cacheDir
-    prepareBuildPackages verbosity opts
+    prepareBuildPackages opts config
 
     flip finally persist $ httpSession verbosity $ do
         -- Make sure we authenticate to Hackage
-        setAuthorityGen $ provideAuthInfo (srcURI opts) $ Just (username opts, password opts)
+        setAuthorityGen $ provideAuthInfo (bc_srcURI config) $ Just (bc_username config, bc_password config)
 
         -- Find those files *not* marked as having documentation in our cache
-        index <- downloadIndex (srcURI opts) cacheDir
+        index <- downloadIndex (bc_srcURI config) cacheDir
         to_build <- filterM does_not_have_docs
-                            [pkg_id | PkgIndexInfo pkg_id _ _ _ <- index
-                            , null (selectedPkgs opts) ||
-                              pkg_id `elem` selectedPkgs opts]
+                            [ pkg_id | PkgIndexInfo pkg_id _ _ _ <- index
+                            , null pkgs || pkg_id `elem` pkgs]
         liftIO $ notice verbosity $ show (length to_build) ++ " packages to build documentation for."
       
         -- Try to build each of them, uploading the documentation and
@@ -79,23 +140,17 @@ buildOnce verbosity opts = do
         -- we don't want to keep continually trying to build a failing
         -- package!
         forM_ to_build $ \pkg_id -> do
-            buildPackage verbosity opts pkg_id
+            buildPackage verbosity opts config pkg_id
             liftIO $ mark_as_having_docs pkg_id
-  where
-    cacheDir = stateDir opts </> cacheDirName
-    cacheDirName | URI { uriAuthority = Just auth } <- srcURI opts
-                 = makeValid (uriRegName auth ++ uriPort auth)
-                 | otherwise
-                 = error $ "unexpected URI " ++ show (srcURI opts)
 
 -- Builds a little memoised function that can tell us whether a
 -- particular package already has documentation
-mkPackageHasDocs :: BuildOpts
+mkPackageHasDocs :: BuildOpts -> BuildConfig
                  -> IO (PackageId -> HttpSession Bool,
                         PackageId -> IO (),
                         IO ())
-mkPackageHasDocs opts = do
-    init_already_built <- readBuiltCache (stateDir opts)
+mkPackageHasDocs opts config = do
+    init_already_built <- readBuiltCache (bo_stateDir opts)
     cache_var <- newIORef init_already_built
 
     let mark_as_having_docs pkg_id = atomicModifyIORef cache_var $ \already_built -> (S.insert pkg_id already_built, ())
@@ -104,10 +159,10 @@ mkPackageHasDocs opts = do
             if has_docs
              then return False
              else do
-              has_no_docs <- liftM isNothing $ requestGET' (srcURI opts <//> "package" </> display pkg_id </> "doc")
+              has_no_docs <- liftM isNothing $ requestGET' (bc_srcURI config <//> "package" </> display pkg_id </> "doc")
               unless has_no_docs $ liftIO $ mark_as_having_docs pkg_id
               return has_no_docs
-        persist = readIORef cache_var >>= writeBuiltCache (stateDir opts)
+        persist = readIORef cache_var >>= writeBuiltCache (bo_stateDir opts)
 
     return (does_not_have_docs, mark_as_having_docs, persist)
   where
@@ -122,25 +177,27 @@ mkPackageHasDocs opts = do
     writeBuiltCache cache_dir pkgs = writeFile (cache_dir </> "built") $ unlines $ map display $ S.toList pkgs
 
 
-prepareBuildPackages :: Verbosity -> BuildOpts -> IO ()
-prepareBuildPackages verbosity opts = withCurrentDirectory (stateDir opts) $ do
-    writeFile "config" $ unlines [
-        "remote-repo: " ++ srcName opts ++ ":" ++ show (srcURI opts <//> "packages" </> "archive"),
-        "remote-repo-cache: " ++ stateDir opts </> "packages",
+prepareBuildPackages :: BuildOpts -> BuildConfig -> IO ()
+prepareBuildPackages opts config
+ = withCurrentDirectory (bo_stateDir opts) $ do
+    writeFile "cabal-config" $ unlines [
+        "remote-repo: " ++ srcName config ++ ":" ++ show (bc_srcURI config <//> "packages" </> "archive"),
+        "remote-repo-cache: " ++ bo_stateDir opts </> "packages",
         "library-for-ghci: False",
-        "package-db: " ++ stateDir opts </> "local.conf.d",
+        "package-db: " ++ bo_stateDir opts </> "local.conf.d",
         "documentation: True",
         "remote-build-reporting: detailed",
-        -- Used for the "upload" commands only, not "report" :-(
-        "username: " ++ username opts,
-        "password: " ++ password opts
+        -- TODO: These are currently only used for the "upload" commands
+        --       only, not "report"
+        "username: " ++ bc_username config,
+        "password: " ++ bc_password config
       ]
 
     -- Create cache for the empty configuration directory
     local_conf_d_exists <- doesDirectoryExist "local.conf.d"
     unless local_conf_d_exists $ do
         createDirectory "local.conf.d"
-        let packageConf = stateDir opts </> "local.conf.d"
+        let packageConf = bo_stateDir opts </> "local.conf.d"
         ph <- runProcess "ghc-pkg"
                          ["recache", "--package-conf=" ++ packageConf]
                          Nothing Nothing Nothing Nothing Nothing
@@ -148,22 +205,23 @@ prepareBuildPackages verbosity opts = withCurrentDirectory (stateDir opts) $ do
         _ <- waitForProcess ph
         return ()
     
-    update_ec <- cabal verbosity opts "update" []
+    update_ec <- cabal opts "update" []
     unless (update_ec == ExitSuccess) $
         die "Could not 'cabal update' from specified server"
 
 
-buildPackage :: Verbosity -> BuildOpts -> PackageId -> HttpSession ()
-buildPackage verbosity opts pkg_id = do
+buildPackage :: Verbosity -> BuildOpts -> BuildConfig -> PackageId
+             -> HttpSession ()
+buildPackage verbosity opts config pkg_id = do
     -- The documentation is installed within the stateDir because we
     -- set a prefix while installing
-    let doc_root = stateDir opts </> "share" </> "doc"
+    let doc_root = bo_stateDir opts </> "share" </> "doc"
         doc_dir      = doc_root </> display pkg_id </> "html"
         temp_doc_dir = doc_root </> display pkg_id </> display (pkgName pkg_id)
         --versionless_pkg_url = srcURI opts <//> "package" </> "$pkg"
-        pkg_url = srcURI opts <//> "package" </> "$pkg-$version"
+        pkg_url = bc_srcURI config <//> "package" </> "$pkg-$version"
 
-    mb_docs <- liftIO $ withCurrentDirectory (stateDir opts) $ do
+    mb_docs <- liftIO $ withCurrentDirectory (bo_stateDir opts) $ do
         -- Let's not clean in between installations. This should save us
         -- some download/installation time for packages that are
         -- depended on by a lot of things, at the cost of some disk
@@ -176,7 +234,7 @@ buildPackage verbosity opts pkg_id = do
         -- only generates build reports if you are building from a
         -- tarball that was verifiably downloaded from the server
         -- TODO: Why do we ignore the result code here?
-        void $ cabal verbosity opts "install"
+        void $ cabal opts "install"
                      ["--enable-documentation",
                       "--enable-tests",
                       -- We know where this documentation will
@@ -193,14 +251,14 @@ buildPackage verbosity opts pkg_id = do
                       "--haddock-contents-location=" ++ show pkg_url,
                       -- Link to colourised source code:
                       "--haddock-hyperlink-source",
-                      "--prefix=" ++ stateDir opts,
+                      "--prefix=" ++ bo_stateDir opts,
                       display pkg_id]
 
         -- Submit a report even if installation/tests failed: all
         -- interesting data points!
-        report_ec <- cabal verbosity opts "report"
-                           ["--username", username opts,
-                            "--password", password opts]
+        report_ec <- cabal opts "report"
+                           ["--username", bc_username config,
+                            "--password", bc_password config]
 
         -- Delete reports after submission because we don't want to
         -- submit them *again* in the future
@@ -208,10 +266,10 @@ buildPackage verbosity opts pkg_id = do
             -- This seems like a bit of a mess: some data goes into the
             -- user directory:
             dotCabal <- getAppUserDataDirectory "cabal"
-            handleDoesNotExist (return ()) $ removeDirectoryRecursive (dotCabal </> "reports" </> srcName opts)
+            handleDoesNotExist (return ()) $ removeDirectoryRecursive (dotCabal </> "reports" </> srcName config)
             handleDoesNotExist (return ()) $ removeDirectoryRecursive (dotCabal </> "logs")
             -- Other data goes into a local file storing build reports:
-            let simple_report_log = stateDir opts </> "packages" </> srcName opts </> "build-reports.log"
+            let simple_report_log = bo_stateDir opts </> "packages" </> srcName config </> "build-reports.log"
             handleDoesNotExist (return ()) $ removeFile simple_report_log
       
         docs_generated <- doesDirectoryExist doc_dir
@@ -233,15 +291,17 @@ buildPackage verbosity opts pkg_id = do
     case mb_docs of
         Nothing       -> return ()
         Just docs_tgz -> do
-            requestPUT (srcURI opts <//> "package" </> display pkg_id </> "doc") "application/x-gzip" docs_tgz
+            requestPUT (bc_srcURI config <//> "package" </> display pkg_id </> "doc") "application/x-gzip" docs_tgz
 
-cabal :: Verbosity -> BuildOpts -> String -> [String] -> IO ExitCode
-cabal verbosity opts cmd args = do
+cabal :: BuildOpts -> String -> [String] -> IO ExitCode
+cabal opts cmd args = do
     cwd' <- getCurrentDirectory
-    let all_args = ("--config-file=" ++ stateDir opts </> "config"):cmd:args
-    notice verbosity ("In " ++ cwd' ++ ":\n" ++
-                      showCommandForUser "cabal" all_args)
-    ph <- runProcess "cabal" all_args (Just cwd') Nothing Nothing Nothing Nothing
+    let cabalConfigFile = bo_stateDir opts </> "cabal-config"
+        all_args = ("--config-file=" ++ cabalConfigFile) : cmd : args
+    notice (bo_verbosity opts) ("In " ++ cwd' ++ ":\n" ++
+                                showCommandForUser "cabal" all_args)
+    ph <- runProcess "cabal" all_args (Just cwd')
+                     Nothing Nothing Nothing Nothing
     waitForProcess ph
 
 tarGzDirectory :: FilePath -> IO BS.ByteString
@@ -292,56 +352,47 @@ buildFlagDescrs =
       "Where to put files during building"
   ]
 
-validateOpts :: [String] -> IO (Verbosity, BuildOpts)
+validateOpts :: [String] -> IO (Mode, BuildOpts)
 validateOpts args = do
     let (flags0, args', errs) = getOpt Permute buildFlagDescrs args
         flags = accum flags0 emptyBuildFlags
 
-    when (flagHelp flags) printUsage
-    when (not (null errs)) (printErrors errs)
+        stateDir = fromMaybe "build-cache" (flagCacheDir flags)
 
-    case args' of
-      (from:pkgstrs) -> case validateHackageURI from of
-        Left theError -> die theError
-        Right fromURI -> do
-          pkgs <- case validatePackageIds pkgstrs of
-            Left theError -> die theError
-            Right pkgs -> return pkgs
+        opts = BuildOpts {
+                   bo_verbosity = flagVerbosity flags,
+                   bo_stateDir  = stateDir
+               }
 
-          (usrnme, psswrd) <- case extractURICredentials fromURI of
-            Nothing    -> die "No Hackage server username/password"
-            Just creds -> return creds
+        mode = case args' of
+               _ | flagHelp flags -> Help []
+                 | not (null errs) -> Help errs
+               ["init", uri] ->
+                   -- We don't actually want the URI at this point
+                   -- (see [Note: Show/Read URI])
+                   case validateHackageURI uri of
+                   Left  err -> Help [err]
+                   Right _   -> Init uri
+               "init" : _ ->
+                   Help ["init takes a single argument (repo URL)"]
+               "build" : pkgstrs ->
+                   case validatePackageIds pkgstrs of
+                   Left  err  -> Help [err]
+                   Right pkgs -> Build pkgs
+               cmd : _ -> Help ["Unrecognised command: " ++ show cmd]
+               [] -> Help []
 
-          -- Ensure we store the absolute state_dir, because we might
-          -- change the CWD later and we don't want the stateDir to be
-          -- invalidated by such a change
-          --
-          -- We have to ensure the directory exists before we do
-          -- canonicalizePath, or otherwise we get an exception if it
-          -- does not yet exist
-          let rel_state_dir = fromMaybe "build-cache" (flagCacheDir flags)
-          createDirectoryIfMissing False rel_state_dir
-          abs_state_dir <- canonicalizePath rel_state_dir
+    -- Ensure we store the absolute state_dir, because we might
+    -- change the CWD later and we don't want the stateDir to be
+    -- invalidated by such a change
+    --
+    -- We have to ensure the directory exists before we do
+    -- canonicalizePath, or otherwise we get an exception if it
+    -- does not yet exist
 
-          return $ (,) (flagVerbosity flags) BuildOpts {
-            srcURI       = removeURICredentials fromURI,
-            username     = usrnme,
-            password     = psswrd,
-            stateDir     = abs_state_dir,
-            selectedPkgs = pkgs
-          }
-
-      _ -> do putStrLn "expected a URL: the Hackage server to build for"
-              printUsage
+    return (mode, opts)
 
   where
-    printUsage = do
-        putStrLn $ usageInfo usageHeader buildFlagDescrs
-        exitSuccess
-    usageHeader = "Usage: hackage-build URL [packages] [options]\nOptions:"
-    printErrors errs = do
-        putStrLn $ concat errs ++ "Try --help."
-        exitFailure
 
     accum flags = foldr (flip (.)) id flags
 
