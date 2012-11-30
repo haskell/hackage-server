f64c35f71c3936d46bd87755130283ae7d54457c
Duncan Coutts <duncan@well-typed.com>
2012-11-30 17:46:53 +0000

    Add script to get all the old hackage data

---
+++ old-hackage-download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to get all the old hackage data
+
+IMPORTDATA_DIR=./import-data
+
+mkdir -p ${IMPORTDATA_DIR}/passwd/ ${IMPORTDATA_DIR}/archive/
+
+echo "Syncing passwd files..."
+rsync -r -v hackage.haskell.org:/srv/www/hackage.haskell.org/passwd/ ${IMPORTDATA_DIR}/passwd/
+
+echo "Syncing archive..."
+rsync -r -v -f'- latest/' -f'- doc/' -f'- logs/' hackage.haskell.org:/srv/www/hackage.haskell.org/public_html/packages/archive/ ${IMPORTDATA_DIR}/archive/
+
