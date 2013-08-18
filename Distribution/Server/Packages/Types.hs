6f5e6a853869c0826663b71559b1c2436bec3e6d
Duncan Coutts <duncan@well-typed.com>
2013-08-18 18:45:00 +0000

    Trivial tidy-up change

--- Distribution/Server/Packages/Types.hs
+++ Distribution/Server/Packages/Types.hs
@@ -89,10 +89,6 @@ data PkgTarball = PkgTarball {
    pkgTarballNoGz :: !BlobId
 } deriving (Eq, Typeable, Show)
 
-instance SafeCopy PkgTarball where
-  putCopy = contain . Serialize.put
-  getCopy = contain Serialize.get
-
 type UploadInfo = (UTCTime, UserId)
 
 pkgUploadTime :: PkgInfo -> UTCTime
@@ -140,6 +136,10 @@ instance Serialize PkgTarball where
           pkgTarballGz = gz,
           pkgTarballNoGz = noGz
       }
+
+instance SafeCopy PkgTarball where
+  putCopy = contain . Serialize.put
+  getCopy = contain Serialize.get
 
 instance MemSize PkgInfo where
     memSize (PkgInfo a b c d e) = memSize5 a b c d e
