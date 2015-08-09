09afaa3a43ba9bb6d5d305b1cc7c2f1744fb9154
Edsko de Vries <edsko@well-typed.com>
2015-08-09 12:55:53 +0100

    Make parseRead/parseText show type
    This makes debugging a bit easier; interpreting
    
    ```
    Error importing 'test-dump/upload/maintainers.csv' :Unable to 'read' user id
    "1" as type UserId
    ```
    
    is a whole lot easier than the same error message without the type information
    (UserId).
    
    This comment also make the error message mention whether we were using 'read'
    or 'simpleParse'.

--- Distribution/Server/Util/CountingMap.hs
+++ Distribution/Server/Util/CountingMap.hs
@@ -54,7 +54,7 @@ class CountingMap k a | a -> k where
   cmToCSV        :: a -> CSV
   cmInsertRecord :: Monad m => Record -> a -> m (a, Int)
 
-instance (Ord k, Text k) => CountingMap k (SimpleCountingMap k) where
+instance (Ord k, Typeable k, Text k) => CountingMap k (SimpleCountingMap k) where
   cmEmpty = SCM (NCM 0 Map.empty)
 
   cmTotal (SCM (NCM total _)) = total
@@ -81,7 +81,7 @@ instance (Ord k, Text k) => CountingMap k (SimpleCountingMap k) where
   cmInsertRecord _ _ =
     fail "cmInsertRecord: Invalid record"
 
-instance (Text k, Ord k, Eq l, CountingMap l a) => CountingMap (k, l) (NestedCountingMap k a) where
+instance (Typeable k, Text k, Ord k, Eq l, CountingMap l a) => CountingMap (k, l) (NestedCountingMap k a) where
   cmEmpty = NCM 0 Map.empty
 
   cmTotal (NCM total _m) = total
