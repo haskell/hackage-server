60eaf4abffca3ca33d7fa31820610fb9ba8689e6
Duncan Coutts <duncan@community.haskell.org>
2016-05-30 11:26:55 +0100

    Also serve NEWS files as a version history
    If we have both NEWS and ChangeLog, the package is probably following
    the GNU convention in which case we should prefer the NEWS as it's the
    user visible changes, rather than every commit.

--- Distribution/Server/Packages/ChangeLog.hs
+++ Distribution/Server/Packages/ChangeLog.hs
@@ -12,5 +12,5 @@ isChangeLogFile fname = map Char.toLower base `elem` basenames
                         && ext `elem` extensions
   where
     (base, ext) = splitExtension fname
-    basenames  = ["changelog", "change_log", "changes"]
+    basenames  = ["news", "changelog", "change_log", "changes"]
     extensions = ["", ".txt", ".md", ".markdown"]
