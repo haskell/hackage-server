02b961c4a28ac1ea9556e0dd1c9d8c618cf3e8b6
Alexander Thiemann <mail@athiemann.net>
2016-09-04 15:59:15 +0200

    add missing derive data typeable

--- Distribution/Server/Util/Nonce.hs
+++ Distribution/Server/Util/Nonce.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Distribution.Server.Util.Nonce
     ( newRandomNonce
     , renderNonce, parseNonce, parseNonceM
@@ -11,21 +12,22 @@ where
 
 import Distribution.Server.Framework.MemSize
 
-import qualified Data.Char as Char
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BS -- Only used for ASCII data
-import qualified Data.ByteString.Base16 as Base16
-import Data.Typeable
 import Data.SafeCopy (base, extension, deriveSafeCopy, Migrate(..))
+import Data.Typeable
 import System.IO
+import qualified Data.ByteString.Base16 as Base16
+import qualified Data.ByteString.Char8 as BS -- Only used for ASCII data
+import qualified Data.Char as Char
 
 newtype Nonce = Nonce ByteString
   deriving (Eq, Ord, Show, Typeable, MemSize)
 
 newRandomNonce :: Int -> IO Nonce
 newRandomNonce len = do
-  raw <- withFile "/dev/urandom" ReadMode $ \h ->
-           BS.hGet h len
+  raw <-
+      withFile "/dev/urandom" ReadMode $ \h ->
+      BS.hGet h len
   return $! Nonce raw
 
 getRawNonceBytes :: Nonce -> ByteString
