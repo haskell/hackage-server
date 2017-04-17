d5abd9796fc5d00b031052478c00e6b6138a16ab
Herbert Valerio Riedel <hvr@gnu.org>
2017-04-17 16:12:17 +0200

    Candidate banner: link to the uploading docs
    The plan is to augment the uploading page with relevant information
    for package maintainers that would be overkill to include on the
    candidate preview page

--- Distribution/Server/Pages/Package.hs
+++ Distribution/Server/Pages/Package.hs
@@ -98,7 +98,12 @@ packagePage render headLinks top sections
     candidateBanner
       | isCandidate = [ thediv ! [theclass "candidate-info"]
                         << [ paragraph << [ strong (toHtml "This is a package candidate release!")
-                                          , toHtml " Here you can preview how this package release will appear once published to the main package index (which can be accomplished via the 'maintain' link below). Please note that once a package has been published to the main package index it cannot be undone!" ] ] ]
+                                          , toHtml " Here you can preview how this package release will appear once published to the main package index (which can be accomplished via the 'maintain' link below)."
+                                          , toHtml " Please note that once a package has been published to the main package index it cannot be undone!"
+                                          , toHtml " Please consult the "
+                                          , anchor ! [href "/upload"] << "package uploading documentation"
+                                          , toHtml " for more information."
+                                          ] ] ]
       | otherwise = []
 
     renderHeads = case headLinks of
