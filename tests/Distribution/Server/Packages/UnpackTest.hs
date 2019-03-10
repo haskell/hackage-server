ea52daeca2ee67273a5a5256bd1a50718451583c
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-03-10 17:08:36 +0200

    Update tasty

--- tests/Distribution/Server/Packages/UnpackTest.hs
+++ tests/Distribution/Server/Packages/UnpackTest.hs
@@ -3,6 +3,7 @@ module Distribution.Server.Packages.UnpackTest (
   testPermissions,
   ) where
 
+import Control.Monad (when)
 import qualified Codec.Archive.Tar       as Tar
 import qualified Codec.Archive.Tar.Check as Tar
 import qualified Codec.Compression.GZip as GZip
@@ -10,8 +11,7 @@ import qualified Data.ByteString.Lazy as BL
 
 import Distribution.Server.Packages.Unpack
 
-import Control.Monad (when)
-import Test.HUnit
+import Test.HUnit (Assertion, assertFailure)
 
 deriving instance Eq Tar.PortabilityError
 deriving instance Eq Tar.FileNameError
@@ -20,8 +20,8 @@ deriving instance Eq CombinedTarErrs
 -- | Test that check permissions does the right thing
 testPermissions :: FilePath ->  -- ^ .tar.gz file to test
   (Tar.Entry -> Maybe CombinedTarErrs) ->  -- ^ Converter to create errors if necessary
-  Test
-testPermissions tarPath mangler = TestCase $ do
+  Assertion
+testPermissions tarPath mangler = do
     entries <- return . Tar.read . GZip.decompress =<< BL.readFile tarPath
     let mappedEntries = Tar.foldEntries Tar.Next Tar.Done (Tar.Fail . FormatError) entries
     when ((checkEntries mangler mappedEntries) /= checkUselessPermissions mappedEntries) $
