d2cd6b1108225445455df101b706ce2dc044093d
Evan Relf <evan@awakesecurity.com>
2021-08-02 10:18:45 -0700

    Add Nix shell with required dependencies
    Allows you to get up and running more easily if you have the Nix package
    manager installed:
    
    ```bash
    $ nix-shell --pure
    [nix-shell]$ cabal update
    [nix-shell]$ cabal build
    ```

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    # master on 2021-08-02
+    url = "https://github.com/NixOS/nixpkgs/archive/70e001f35cc363eb789ea0a04eff11b86c440ba3.tar.gz";
+    sha256 = "1mrhbcfa8kkx1qnax8xh41grinqiycl56wlws5vvrli8w0pzgl1r";
+  };
+
+  pkgs = import nixpkgs { config = { }; };
+
+in
+pkgs.mkShell {
+  buildInputs = [
+    # Haskell development
+    pkgs.cabal-install
+    pkgs.ghc
+
+    # Dependencies
+    pkgs.icu
+    pkgs.zlib
+  ];
+}
