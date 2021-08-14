ab95c0e0a5c9a1b8840e5286c8e6839ae01360de
zypeh <zypeh@users.noreply.github.com>
2021-08-14 17:39:03 +0800

    Add check status page link to hackage 503 page

--- Distribution/Server.hs
+++ Distribution/Server.hs
@@ -356,7 +356,7 @@ html503 =
     "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" ++
     "<html><head><title>503 Service Unavailable</title></head><body><h1>" ++
     "503 Service Unavailable</h1><p>The server is undergoing maintenance" ++
-    "<br>It'll be back soon</p></body></html>"
+    "<br>It'll be back soon. <a href=\"https://status.haskell.org\">Check status.haskell.org</a></p></body></html>"
 
 tearDownTemp :: TempServer -> IO ()
 tearDownTemp (TempServer tid) = do
