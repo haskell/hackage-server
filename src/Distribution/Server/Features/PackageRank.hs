54de869d5baf0176a843b0b86d1b3ec733e7ec6a
kubaneko <kubanek0ondrej@gmail.com>
2022-07-03 21:37:37 +0200

    write out ranking criteria

--- src/Distribution/Server/Features/PackageRank.hs
+++ src/Distribution/Server/Features/PackageRank.hs
@@ -3,7 +3,15 @@ module Distribution.Server.Features.PackageRank (
   ) where
 
 import Distribution.Package
-import Distribution.Server.Packages.Types
 
 rankPackage :: (Package a) => a -> IO Double
-rankPackage p=return 0
+rankPackage p=return$ reverseDeps+usageTrend+docScore+stability
+                +authNum+goodMetadata+weightUniqueDeps+activelyMaintained
+    where   reverseDeps=1
+            usageTrend=1
+            docScore=1
+            stability=1
+            authNum=1
+            goodMetadata=1
+            weightUniqueDeps=1
+            activelyMaintained=1
