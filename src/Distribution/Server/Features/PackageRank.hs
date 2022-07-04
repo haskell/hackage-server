1e2c6c8f618e6f26c78e18544cd7f54c7ab7fd41
kubaneko <kubanek0ondrej@gmail.com>
2022-07-04 23:22:32 +0200

    started with maintainer number

--- src/Distribution/Server/Features/PackageRank.hs
+++ src/Distribution/Server/Features/PackageRank.hs
@@ -3,15 +3,25 @@ module Distribution.Server.Features.PackageRank (
   ) where
 
 import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.Server.Features.Upload
+import Distribution.Server.Users.UserIdSet as UserIdSet
 
-rankPackage :: (Package a) => a -> IO Double
-rankPackage p=return$ reverseDeps+usageTrend+docScore+stability
-                +authNum+goodMetadata+weightUniqueDeps+activelyMaintained
+rankPackage :: PackageDescription -> IO Double
+rankPackage p=do
+                maintainers <- maintNum
+                return maintainers+reverseDeps+usageTrend+docScore+stability
+                    +goodMetadata+weightUniqueDeps+activelyMaintained
     where   reverseDeps=1
             usageTrend=1
             docScore=1
             stability=1
-            authNum=1
+            maintNum :: IO Double
+            maintNum=do  
+                        maintSet<-queryUserGroup$maintainersGroupDescription pkgNm
+                        return fromInteger.UserIdSet$size maintSet
             goodMetadata=1
             weightUniqueDeps=1
             activelyMaintained=1
+            pkgNm :: PackageName
+            pkgNm=pkgName$package p
