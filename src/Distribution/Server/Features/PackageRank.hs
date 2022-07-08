056c83331c627dcf38f71d8c2e48f620d6912f7d
kubaneko <kubanek0ondrej@gmail.com>
2022-07-08 23:06:24 +0200

    formatted a bit

--- src/Distribution/Server/Features/PackageRank.hs
+++ src/Distribution/Server/Features/PackageRank.hs
@@ -8,27 +8,35 @@ import Distribution.Server.Users.Group
 import Distribution.Server.Features.Upload
 import Distribution.Server.Features.DownloadCount
 
-rankPackageIO download upload p=maintNum
+rankPackageIO download upload p = maintNum
     where
+            -- Number of maintainers
             maintNum :: IO Double
-            maintNum=do  
-                        maint<-queryUserGroups$[maintainersGroup upload pkgNm]
-                        return.fromInteger.toInteger$size maint
+            maintNum = do  
+                        maint <- queryUserGroups $ 
+                                [maintainersGroup upload pkgNm]
+                        return . fromInteger . toInteger $ size maint
             pkgNm :: PackageName
-            pkgNm=pkgName$package p
-rankPackagePure p=reverseDeps+usageTrend+docScore+stabilityScore
+            pkgNm = pkgName $ package p
+
+rankPackagePure p = reverseDeps+usageTrend+docScore+stabilityScore
                     +goodMetadata+weightUniqueDeps+activelyMaintained
-    where   reverseDeps=1
-            usageTrend=1
-            docScore=1
-            stabilityScore=1
-            testsBench=(bool2Double.hasTests) p + (bool2Double.hasBenchmarks) p
-            goodMetadata=1
-            weightUniqueDeps=1
-            activelyMaintained=1
+    where   reverseDeps = 1
+            usageTrend = 1
+            docScore = 1
+            stabilityScore = 1
+            -- Does the Package have tests and Benchmarks
+            testsBench = (bool2Double . hasTests) p 
+                            + (bool2Double . hasBenchmarks) p
+            goodMetadata = 1
+            weightUniqueDeps = 1
+            activelyMaintained = 1
             bool2Double :: Bool -> Double
-            bool2Double true=1
-            bool2Double false=0
+            bool2Double true = 1
+            bool2Double false = 0
 
 rankPackage :: DownloadFeature -> UploadFeature -> PackageDescription -> IO Double
-rankPackage download upload p=rankPackageIO download upload p>>=(\x->return$x + rankPackagePure p)
+rankPackage download upload p = rankPackageIO download upload p 
+                                >>= (\x->return$x + rankPackagePure p)
+
+
