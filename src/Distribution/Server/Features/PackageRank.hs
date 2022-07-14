1a157b8452046cca05ce73fd349ceae3bbb63ce5
kubaneko <kubanek0ondrej@gmail.com>
2022-07-14 21:46:06 +0200

    more work on versions

--- src/Distribution/Server/Features/PackageRank.hs
+++ src/Distribution/Server/Features/PackageRank.hs
@@ -5,8 +5,8 @@ module Distribution.Server.Features.PackageRank
 import           Distribution.Package
 import           Distribution.PackageDescription
 import           Distribution.Server.Features.DownloadCount
-import           Distribution.Server.Features.PackageInfoJSON.State
-                                                ( getVersionsFor )
+import           Distribution.Server.Features.HaskellPlatform
+import           Distribution.Server.Features.PreferredVersions
 import           Distribution.Server.Features.Upload
 import           Distribution.Server.Users.Group
                                                 ( queryUserGroups
@@ -21,40 +21,47 @@ data Scorer = Scorer
   , score :: Double
   }
 
-instance Num Scorer where
-  Scorer a b + Scorer c d = Scorer (a + c) (b + d)
+add (Scorer a b) (Scorer c d) = Scorer (a + c) (b + d)
 
-rankPackageIO download upload p = maintNum
+rankPackageIO
+  :: VersionsFeature
+  -> PlatformFeature
+  -> DownloadFeature
+  -> UploadFeature
+  -> PackageDescription
+  -> IO Double
+rankPackageIO prefferedV platform download upload p = maintNum
  where
+  pkgNm :: PackageName
+  pkgNm = pkgName $ package p
   -- Number of maintainers
   maintNum :: IO Double
   maintNum = do
     maint <- queryUserGroups [maintainersGroup upload pkgNm]
     return . fromInteger . toInteger $ size maint
-  versionsPkg :: IO Double
-  versionsPkg = getVersionsFor pkgNm >>= return length
-  pkgNm :: PackageName
-  pkgNm = pkgName $ package p
+  versions = platformVersions platform pkgNm
 
-rankPackagePure p =
-  reverseDeps
-    + usageTrend
-    + docScore
-    + reverseDeps
+rankPackagePure p = reverseDeps + usageTrend + docScore + reverseDeps
  where
-  reverseDeps        = 1
-  dependencies       = allBuildDepends p
-  usageTrend         = 1
-  docScore           = 1
-  testsBench = (bool2Double . hasTests) p + (bool2Double . hasBenchmarks) p
-  isApp              = (isNothing . library) p && (not . null . executables) p
+  reverseDeps  = 1
+  dependencies = allBuildDepends p
+  usageTrend   = 1
+  docScore     = 1
+  testsBench   = (bool2Double . hasTests) p + (bool2Double . hasBenchmarks) p
+  isApp        = (isNothing . library) p && (not . null . executables) p
   bool2Double :: Bool -> Double
   bool2Double true  = 1
   bool2Double false = 0
 
 rankPackage
-  :: DownloadFeature -> UploadFeature -> PackageDescription -> IO Double
-rankPackage download upload p =
-  rankPackageIO download upload p >>= (\x -> return $ x + rankPackagePure p)
+  :: VersionsFeature
+  -> PlatformFeature
+  -> DownloadFeature
+  -> UploadFeature
+  -> PackageDescription
+  -> IO Double
+rankPackage versions platform download upload p =
+  rankPackageIO versions platform download upload p
+    >>= (\x -> return $ x + rankPackagePure p)
 
 
