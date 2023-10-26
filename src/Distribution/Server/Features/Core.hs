113d4dfb432c63bc829e27d417058c9f86ddcf64
Vladimir Nikishkin <>
2023-10-26 13:11:23 +0800

    Allow serving cabal files with package name in the file name.
    Allow serving url like this:
    https://hackage.haskell.org/package/pkg-2.6/revision/pkg-2.6-1.cabal

--- src/Distribution/Server/Features/Core.hs
+++ src/Distribution/Server/Features/Core.hs
@@ -225,6 +225,7 @@ data CoreResource = CoreResource {
     corePackageTarball  :: Resource,
     -- | A Cabal file metatada revision.
     coreCabalFileRev    :: Resource,
+    coreCabalFileRevName    :: Resource,
 
     -- Rendering resources.
     -- | URI for `corePackagesPage`, given a format (blank for none).
@@ -403,6 +404,7 @@ coreFeature ServerEnv{serverBlobStore = store} UserFeature{..}
           , coreCabalFile
           , coreCabalFileRevs
           , coreCabalFileRev
+          , coreCabalFileRevName
           , coreUserDeauth
           , coreAdminDeauth
           , corePackUserDeauth
@@ -456,6 +458,11 @@ coreFeature ServerEnv{serverBlobStore = store} UserFeature{..}
         resourceDesc = [(GET, "Get package .cabal file revision")]
       , resourceGet  = [("cabal", serveCabalFileRevision)]
       }
+    coreCabalFileRevName = (resourceAt "/package/:package/revision/:tarball-:revision.:format") {
+        resourceDesc = [(GET, "Get package .cabal file revision with name")]
+      , resourceGet  = [("cabal", serveCabalFileRevisionName)]
+      }
+
 
     coreUserDeauth = (resourceAt "/packages/deauth") {
         resourceDesc = [(GET,  "Deauth Package user")]
@@ -740,6 +747,21 @@ coreFeature ServerEnv{serverBlobStore = store} UserFeature{..}
 
     serveCabalFileRevision :: DynamicPath -> ServerPartE Response
     serveCabalFileRevision dpath = do
+      pkginfo <- packageInPath dpath >>= lookupPackageId
+      let mrev      = lookup "revision" dpath >>= fromReqURI
+          revisions = pkgMetadataRevisions pkginfo
+      case mrev >>= \rev -> revisions Vec.!? rev of
+        Just (fileRev, (utime, _uid)) -> return $ toResponse cabalfile
+          where
+            cabalfile = Resource.CabalFile (cabalFileByteString fileRev) utime
+        Nothing -> errNotFound "Package revision not found"
+                     [MText "Cannot parse revision, or revision out of range."]
+
+    serveCabalFileRevisionName :: DynamicPath -> ServerPartE Response
+    serveCabalFileRevisionName dpath = do
+      pkgid1 <- packageTarballInPath dpath
+      pkgid2 <- packageInPath dpath
+      guard (pkgVersion pkgid2 == pkgVersion pkgid2)
       pkginfo <- packageInPath dpath >>= lookupPackageId
       let mrev      = lookup "revision" dpath >>= fromReqURI
           revisions = pkgMetadataRevisions pkginfo
