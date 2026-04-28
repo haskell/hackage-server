ec9f5dc4d88aba8090003187d9d33432b89c6403
Sandy Maguire <sandy@sandymaguire.me>
2026-04-28 09:48:42 -0700

    Remove unpreferred

--- src/Distribution/Server/Features/PreferredVersions/Types.hs
+++ src/Distribution/Server/Features/PreferredVersions/Types.hs
@@ -2,8 +2,4 @@
 
 module Distribution.Server.Features.PreferredVersions.Types where
 
-import Data.SafeCopy (base, deriveSafeCopy)
-
-data VersionStatus = NormalVersion | DeprecatedVersion | UnpreferredVersion deriving (Show, Eq, Ord, Enum)
-
-$(deriveSafeCopy 0 'base ''VersionStatus)
+data VersionStatus = NormalVersion | DeprecatedVersion deriving (Show, Eq, Ord, Enum)
